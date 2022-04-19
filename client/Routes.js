4117b034964ff2d83f178165696bae8c11b4b0a2
Songyee Han <hansongyee0304@gmail.com>
2022-04-19 12:55:52 -0700

    AllProducts Component Finished

--- client/Routes.js
+++ client/Routes.js
@@ -4,7 +4,7 @@ import {withRouter, Route, Switch, Redirect} from 'react-router-dom'
 import { Login, Signup } from './components/AuthForm';
 import Home from './components/Home';
 import {me} from './store'
-import {AllProducts} from './components/AllProducts'
+import AllProducts from './components/AllProducts'
 
 /**
  * COMPONENT
--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -7,38 +7,44 @@ import { fetchProducts } from "../store/products";
 // (below) is not connected to Redux, while the default export (at the very
 // bottom) is connected to Redux. Our tests should cover _both_ cases.
 export class AllProducts extends React.Component {
-//   componentDidMount() {
-//     this.props.displayProducts()
-//   }
+  componentDidMount() {
+    this.props.displayProducts()
+  }
   render() {
     const { products } = this.props
+    console.log(products)
     return (
     <div>
-        <h1>Hello</h1>
-      {/* <ul>
+        <h1>All Products</h1>
+      <ul>
       { products.map((product) => {
           return (
-            <li key={product.id}> {product.name}</li>
+              <div key={product.id}>
+                <li key={product.id}> <Link to={`/products/${product.id}`} key={product.id}><img src={`${product.imageUrl}`}/> {product.title} </Link></li>
+                <p>{product.price}</p>
+                <button type="button">Add to Cart</button>
+              </div>
           )
+         
         })
         }
-      </ul> */}
+      </ul>
     </div>
 
     )
   }
 }
 
-// const mapState = (state) => {
-//   return {
-//     products: state.products
-//   };
-// };
+const mapState = (state) => {
+  return {
+    products: state.products
+  };
+};
 
-// const mapDispatch = (dispatch) => {
-//   return {
-//     displayProducts: () => dispatch(fetchProducts())
-//   };
-// };
+const mapDispatch = (dispatch) => {
+  return {
+    displayProducts: () => dispatch(fetchProducts())
+  };
+};
 
-// export default connect(mapState, mapDispatch)(AllProducts);
+export default connect(mapState, mapDispatch)(AllProducts);
