d02c7d09c41a2f2111cefa75a06c877c02e8f84f
Songyee Han <hansongyee0304@gmail.com>
2022-04-19 09:21:45 -0700

    AllProducts API and redux

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,13 @@ import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import auth from './auth'
+import AllProductsReducer from './products'
 
-const reducer = combineReducers({ auth })
+const reducer = combineReducers({ 
+  auth,
+  products: AllProductsReducer
+
+ })
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
