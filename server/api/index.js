bc678d19050677d04f219b6110b455136d966c4e
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-04-19 16:40:05 -0400

    categories drop down menu

--- server/api/index.js
+++ server/api/index.js
@@ -1,10 +1,11 @@
-const router = require('express').Router()
-module.exports = router
+const router = require("express").Router();
+module.exports = router;
 
-router.use('/products', require('./products'))
+router.use("/products", require("./products"));
+router.use("/categories", require("./categories"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found')
-  error.status = 404
-  next(error)
-})
+  const error = new Error("Not Found");
+  error.status = 404;
+  next(error);
+});
