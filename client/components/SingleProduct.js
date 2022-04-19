1095195e6c1f510f4e32da08df34cf856776786f
Yeji Jang <yejijang95@gmail.com>
2022-04-19 15:41:25 -0400

    single product with reviews - front & backend

---
+++ client/components/SingleProduct.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { singleProductThunk } from "../store/singleProduct";
+
+class SingleProduct extends Component {
+  componentDidMount() {
+    this.props.loadSingleProdcut(this.props.match.params.productId);
+  }
+
+  render() {
+    const product = this.props.product;
+    const reviews = this.props.product.reviews || [];
+    return (
+      <div className="singleProduct">
+        <h2>{product.title}</h2>
+        <img src={product.imageUrl} />
+        <h3>Price: {product.price}</h3>
+        <p>{product.description}</p>
+        <h3>Reviews:</h3>
+        {reviews.length === 0 ? (
+          <p>No reviews yet.</p>
+        ) : (
+          reviews.map((review) => (
+            <div className="productReviews" key={review.id}>
+              <p>{review.content}</p>
+            </div>
+          ))
+        )}
+      </div>
+    );
+  }
+}
+
+const mapState = (state) => {
+  return {
+    product: state.product,
+  };
+};
+
+const mapDispatch = (dispatch) => {
+  return {
+    loadSingleProdcut: (productId) => dispatch(singleProductThunk(productId)),
+  };
+};
+
+export default connect(mapState, mapDispatch)(SingleProduct);
---
+++ client/store/singleProduct.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+//initial state
+const initialState = {};
+
+//action type
+const SINGLE_PRODUCT = "SINGLE_PRODUCT";
+
+//action creator
+const singleProduct = (product) => ({
+  type: SINGLE_PRODUCT,
+  product,
+});
+
+//thunk creator
+export const singleProductThunk = (productId) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.get(`/api/products/${productId}`);
+      dispatch(singleProduct(data));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+};
+
+//reducer
+export default function singleProductReducer(state = initialState, action) {
+  switch (action.type) {
+    case SINGLE_PRODUCT:
+      return action.product;
+    default:
+      return state;
+  }
+}
