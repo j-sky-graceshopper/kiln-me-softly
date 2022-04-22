74d3e3e29b396059fe62a13785967f1e069bf5d6
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-04-22 12:18:37 -0400

    update single product view to put info on side of picture instead of bottom

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,46 +2,56 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { singleProductThunk } from "../store/singleProduct";
 import AddToCart from "./AddToCart";
-import auth from "../store/auth"
+import auth from "../store/auth";
 
 class SingleProduct extends Component {
   constructor(props) {
-    super(props)
-    this.handleLink = this.handleLink.bind(this)
+    super(props);
+    this.handleLink = this.handleLink.bind(this);
   }
   componentDidMount() {
     this.props.loadSingleProdcut(this.props.match.params.productId);
   }
   handleLink() {
-    const id = this.props.match.params.productId
-    this.props.history.push(`/edit/products/${id}`)
+    const id = this.props.match.params.productId;
+    this.props.history.push(`/edit/products/${id}`);
   }
   render() {
     const product = this.props.product;
     const reviews = this.props.product.reviews || [];
-    const { auth } = this.props
+    const { auth } = this.props;
 
-    
     return (
       <div className="singleProduct">
-        <h2>{product.title}</h2>
         <img src={product.imageUrl} />
-        <h3>Price: ${product.price}</h3>
-        <p>{product.description}</p>
-        <AddToCart product={product} />
-        <h3>Reviews:</h3>
-        {reviews.length === 0 ? (
-          <p>No reviews yet.</p>
-        ) : (
-          reviews.map((review) => (
-            <div className="productReviews" key={review.id}>
-              <p>{review.content}</p>
-            </div>
-          ))
-        )}
+        <div className="singleProduct-info">
+          <h1>{product.title}</h1>
 
-        <div>
-          {auth.isAdmin ? <button className="edit-product" type="button" onClick={this.handleLink}>Edit Product</button>: null}
+          <h2>Price: ${product.price}</h2>
+          <p>{product.description}</p>
+          <AddToCart product={product} />
+          <h2>Reviews:</h2>
+          {reviews.length === 0 ? (
+            <p>No reviews yet.</p>
+          ) : (
+            reviews.map((review) => (
+              <div className="productReviews" key={review.id}>
+                <p>{review.content}</p>
+              </div>
+            ))
+          )}
+
+          <div>
+            {auth.isAdmin ? (
+              <button
+                className="edit-product"
+                type="button"
+                onClick={this.handleLink}
+              >
+                Edit Product
+              </button>
+            ) : null}
+          </div>
         </div>
       </div>
     );
