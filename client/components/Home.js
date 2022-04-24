a92535c1289f091853f3bf631efd361109caca18
Jenny Klein <97071629+jennifermklein@users.noreply.github.com>
2022-04-24 11:57:06 -0400

    homepage design tweaks

--- client/components/Home.js
+++ client/components/Home.js
@@ -15,19 +15,24 @@ export const Home = (props) => {
         id="home-img"
         src="https://img.jakpost.net/c/2020/01/28/2020_01_28_85434_1580208017._large.jpg"
       />
-      <p id="home-text">
-        Welcome{username ? `, ${username},` : null} to Kiln Me Softly, a pottery
-        studio located in Grace Hopper Villiage, New York. Feel free to browse
-        our products, filter by category, or search by item name. <br />
-        <br /> Add items to your cart as you browse, and you can see your cart
-        when you return to the site later on. When you're ready, click checkout
-        and we'll get everything packed and ready for speedy shipment to your
-        home.
+      <span id="home-text">
+        <h1>Welcome{username ? `, ${username},` : null} to Kiln Me Softly.</h1>
         <br />
-        <br /> Don't hestitate to contact us with any questions or requests. We
-        love to speak with our customers, and we hope to see you in the studio
-        some day soon.
-      </p>
+        <p>
+          Kiln me Softly is a pottery studio located in Grace Hopper Villiage,
+          New York. Feel free to browse our products, filter by category, or
+          search by item name. Add items to your cart as you browse, and you can
+          see your cart when you return to the site later on. When you're ready,
+          click checkout and we'll get everything packed and ready for speedy
+          shipment to your home.
+        </p>
+        <p>
+          {" "}
+          Don't hestitate to contact us with any questions or requests. We love
+          to speak with our customers, and we hope to see you in the studio some
+          day soon.
+        </p>
+      </span>
     </div>
   );
 };
